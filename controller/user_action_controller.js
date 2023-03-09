27ff150600b5b18d9fd99a75a6691015910adb12
iHaroon29 <syedharoonali.29@gmail.com>
2023-03-09 11:43:30 +0530

    Image upload service

--- controller/user_action_controller.js
+++ controller/user_action_controller.js
@@ -1,3 +1,4 @@
+const user_image_model = require('../database/models/user_image_model')
 const user_info_model = require('../database/models/user_info_model')
 const user_post_model = require('../database/models/user_post_model')
 module.exports = {
@@ -16,6 +17,21 @@ module.exports = {
       const { userId } = req.params
       const info = await user_info_model.updateInfo({ fullname, about, userId })
       res.status(200).json(info)
+    } catch (e) {
+      res.status(500).send({ message: e.message })
+    }
+  },
+  async saveUserPost(req, res, next) {
+    try {
+      const { jsonData, title, tags } = req.body
+      const { userId } = req.params
+      const newPost = await user_post_model.savePost({
+        jsonData,
+        title,
+        userReferenceId: userId,
+        tags,
+      })
+      res.status(200).send({ post: newPost, message: 'OK' })
     } catch (e) {
       res.status(500).send({ message: e.message })
     }
@@ -52,6 +68,45 @@ module.exports = {
       const { fullname } = req.search
     } catch (e) {
       console.log(e.message)
+      res.status(500).send({ message: e.message })
+    }
+  },
+  async saveUserImage(req, res, next) {
+    try {
+      const { userId } = req.params
+      const newImage = await user_image_model.saveUserImage({
+        imageData: req.file.buffer,
+        userReferenceId: userId,
+      })
+      res.status(200).send({ imageId: newImage._id })
+    } catch (e) {
+      res.status(500).send({ message: e.message })
+    }
+  },
+  async findUserImage(req, res, next) {
+    try {
+      const { imageId } = req.query
+      const { userId } = req.params
+      if (!imageId) return res.status(400).send({ message: 'Invalid Image Id' })
+      const image = await user_image_model.findUserImage({
+        _id: imageId,
+        userReferenceId: userId,
+      })
+      if (!image) return res.status(404).send({ message: 'Image Not Found' })
+      res.set('Content-Type', 'image/png')
+      res.status(200).send(image.imageData)
+    } catch (e) {
+      res.status(500).send({ message: e.message })
+    }
+  },
+  async deleteUserImage(req, res, next) {
+    try {
+      const { imageId } = req.query
+      if (!imageId) return res.status(400).send({ message: 'Invalid Image Id' })
+      const image = await user_image_model.deleteUserImage({ _id: imageId })
+      if (!image) return res.status(404).send({ message: 'Image Not Found' })
+      res.status(200).json(image)
+    } catch (e) {
       res.status(500).send({ message: e.message })
     }
   },
--- routes/user_routes/user_routes.js
+++ routes/user_routes/user_routes.js
@@ -1,15 +1,34 @@
 const router = require('express').Router()
+// const upload = require('multer')({
+//   limits: { fileSize: 1000000 },
+//   fileFilter(req, file, cb) {
+//     if (!file.originalname.match(/\.(png|jpg|jpeg)$/)) {
+//       cb(new Error('Please upload an image.'))
+//     }
+//     cb(undefined, true)
+//   },
+// })
+
 const { authenticate } = require('../../controller/encryption_controller')
 const {
   fetchUserInfo,
   updateUserInfo,
   fetchUserPosts,
   deleteUserPost,
+  saveUserPost,
+  saveUserImage,
+  findUserImage,
+  deleteUserImage,
 } = require('../../controller/user_action_controller')
 
-// Post Routes
+// Image Routes
+router.get('/:userId/image?', [authenticate, findUserImage])
+router.post('/:userId/image', [authenticate, saveUserImage])
+router.delete('/:userId/image?', [authenticate, deleteUserImage])
+
+//Blog Routes
 router.get('/:userId/posts?', [authenticate, fetchUserPosts])
-// router.update('/:userId/post?')
+router.post('/:userId/posts', [saveUserPost])
 router.delete('/:userId/post?', [authenticate, deleteUserPost])
 
 // User Info Routes
